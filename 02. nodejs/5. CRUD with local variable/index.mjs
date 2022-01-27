e05046182e1b6c4f9749828fa3f7358434f1f045
M.Inzamam Malik <malikasinger1@gmail.com>
2022-01-27 20:36:56 +0500

    cors added

--- 02. nodejs/5. CRUD with local variable/index.mjs
+++ 02. nodejs/5. CRUD with local variable/index.mjs
@@ -1,7 +1,9 @@
 import express, { response } from 'express';
+import cors from "cors";
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 var posts = [
     { text: "some post 0" },
