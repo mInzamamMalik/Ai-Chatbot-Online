d6cfb8a349233aca98ffd4e633066fe1d5f408f6
M.Inzamam Malik <malikasinger1@gmail.com>
2022-01-29 22:32:34 +0500

    mongodb

--- 02. nodejs/5. CRUD with local variable/index.mjs
+++ 02. nodejs/5. CRUD with local variable/index.mjs
@@ -3,7 +3,24 @@ import cors from "cors";
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+
+
+// // cors code
+// var whitelist = ['http://example1.com', 'http://example2.com']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions))
+// // cors code end
+
+app.use(cors()); //  allow all origins, not recommended in production
+
 
 var posts = [
     { text: "some post 0" },
@@ -13,26 +30,34 @@ var posts = [
 
 
 app.get('/post/:id', (req, res) => {
-
     const id = Number(req.params.id);
-
     res.send(posts[id]);
 })
+
 app.get('/posts', (req, res) => {
     res.send(posts);
 })
 
 app.post('/post', (req, res) => {
-
     posts.push(req.body);
-
     res.send(`your post is saved 🥳 now we have ${posts.length} posts`)
 })
-app.put('/post', (req, res) => {
-    res.send('I am Express.js server')
+
+app.put('/post/:id', (req, res) => {
+    const id = Number(req.params.id);
+    posts[id] = req.body;
+    res.send(posts[id])
 })
-app.delete('/post', (req, res) => {
-    res.send('I am Express.js server')
+
+app.delete('/post/:id', (req, res) => {
+    const id = Number(req.params.id);
+    delete posts[id];
+    res.send("deleted")
+})
+
+app.delete('/posts', (req, res) => {
+    posts = [];
+    res.send("deleted everything")
 })
 
 const PORT = process.env.PORT || 3000;
